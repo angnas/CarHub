16527e0da5aa8ba5e236f83b8e399873851b78c4
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-05-23 17:26:24 +0200

    develop combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -2,15 +2,44 @@
 import { Combobox, Transition } from "@headlessui/react";
 
 import { SearchManufacturerProps } from "@/types";
+import Image from "next/image";
+import { Fragment, useState } from "react";
+import { manufacturers } from "@/constants";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="search-manufacturer">
       <Combobox>
-        <div className="relative w-full"></div>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}>
+            <Combobox.Options></Combobox.Options>
+          </Transition>
+        </div>
       </Combobox>
     </div>
   );
